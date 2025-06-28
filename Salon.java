93a4dc52c2029967d23e2b619298d552e3706549
ZidaneOT <165101336+ZidaneOT@users.noreply.github.com>
2025-06-28 08:25:34 -0500

    Create Salon.java
    Clase Salon

---
+++ Salon.java
@@ -0,0 +1,11 @@
+public class Salon {
+    String nombre;
+    int capacidad;
+    boolean esLaboratorio;
+
+    public Salon(String nombre, int capacidad, boolean esLaboratorio) {
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.esLaboratorio = esLaboratorio;
+    }
+}
